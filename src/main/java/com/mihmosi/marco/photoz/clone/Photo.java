594c9facec367c151c2b75f5904c527a817d3825
mihmosi <mihmosi@gmail.com>
2023-09-17 13:01:06 +0300

    Initial commit

---
+++ src/main/java/com/mihmosi/marco/photoz/clone/Photo.java
@@ -0,0 +1,38 @@
+package com.mihmosi.marco.photoz.clone;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public class Photo {
+    private String id;
+
+    @NotEmpty
+    private String fileName;
+
+    public Photo() {
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    //raw photo
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+}
